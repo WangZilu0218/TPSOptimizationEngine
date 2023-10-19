3a0f003c29d322ad28385bdecc4bb6c360d3c909
wangzilu <wangzilu@hotmail.com>
2023-10-19 16:32:26 +0800

    10.19

---
+++ enzymeAutoGrad/loss.h
@@ -0,0 +1,10 @@
+//
+// Created by 王子路 on 2023/10/19.
+//
+
+#ifndef OPTIMIZE_ENZYMEAUTOGRAD_LOSS_H_
+#define OPTIMIZE_ENZYMEAUTOGRAD_LOSS_H_
+float calDoseLoss(float *d_dose, float *d_dose_grad, float *d_loss, float dose_value, int size, int sign);
+float calDVHLoss(float *d_dose, float *d_dose_grad, float *d_loss, float d1, int size, float v1, int sign);
+float calgEUDLoss(float *d_dose, float *d_dose_grad, float target, float a, int size, int sign);
+#endif //OPTIMIZE_ENZYMEAUTOGRAD_LOSS_H_
